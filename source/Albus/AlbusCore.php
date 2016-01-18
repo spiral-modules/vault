580e629627a320846323c629fe2cc607ff0863dc
Wolfy-J <wolfy.jd@gmail.com>
2016-01-18 21:02:21 +0300

    Uri generation.

--- source/Albus/AlbusCore.php
+++ source/Albus/AlbusCore.php
@@ -9,6 +9,7 @@ namespace Spiral\Albus;
 
 use Psr\Http\Message\UriInterface;
 use Spiral\Albus\Configs\AlbusConfig;
+use Spiral\Albus\Exceptions\AlbusException;
 use Spiral\Core\Component;
 use Spiral\Core\Container\SingletonInterface;
 use Spiral\Core\ContainerInterface;
@@ -16,8 +17,8 @@ use Spiral\Core\Exceptions\ControllerException;
 use Spiral\Core\HMVC\ControllerInterface;
 use Spiral\Core\HMVC\CoreInterface;
 use Spiral\Debug\Traits\BenchmarkTrait;
+use Spiral\Http\Configs\HttpConfig;
 use Spiral\Http\Routing\RouteInterface;
-use Spiral\Http\Uri;
 use Spiral\Security\Traits\GuardedTrait;
 
 /**
@@ -31,6 +32,11 @@ class AlbusCore extends Component implements CoreInterface, SingletonInterface
      * Declaring to IoC to treat Albus as sinleton.
      */
     const SINGLETON = self::class;
+
+    /**
+     * @var HttpConfig
+     */
+    private $httpConfig = null;
 
     /**
      * @var AlbusConfig
@@ -48,11 +54,18 @@ class AlbusCore extends Component implements CoreInterface, SingletonInterface
     protected $container = null;
 
     /**
+     * AlbusCore constructor.
+     *
+     * @param HttpConfig         $httpConfig
      * @param AlbusConfig        $config
      * @param ContainerInterface $container
      */
-    public function __construct(AlbusConfig $config, ContainerInterface $container)
-    {
+    public function __construct(
+        HttpConfig $httpConfig,
+        AlbusConfig $config,
+        ContainerInterface $container
+    ) {
+        $this->httpConfig = $httpConfig;
         $this->config = $config;
         $this->container = $container;
 
@@ -122,10 +135,35 @@ class AlbusCore extends Component implements CoreInterface, SingletonInterface
      *                            "controller:action" or "controller".
      * @param array|mixed $parameters
      * @return UriInterface
+     * @throws AlbusException
      */
     public function uri($target, $parameters = [])
     {
-        return new Uri('/');
+        $target = str_replace('::', ':', $target);
+
+        $controller = $action = '';
+        if (strpos($target, ':') !== false) {
+            list($controller, $action) = explode(':', $target);
+        } else {
+            $controller = $target;
+
+            if (!empty($parameters)) {
+                throw new AlbusException(
+                    "Unable to generate uri with empty controller action and not empty parameters."
+                );
+            }
+        }
+
+        if (!isset($this->config->getControllers()[$controller])) {
+            throw new AlbusException(
+                "Unable to generate uri, undefined controller '{$controller}'."
+            );
+        }
+
+        $parameters['controller'] = $controller;
+        $parameters['action'] = $action;
+
+        return $this->route->uri($parameters, $this->httpConfig->basePath());
     }
 
     /**
---
+++ source/Albus/Exceptions/AlbusException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+namespace Spiral\Albus\Exceptions;
+
+/**
+ * Albus specific exception.
+ */
+class AlbusException extends \RuntimeException
+{
+
+}
