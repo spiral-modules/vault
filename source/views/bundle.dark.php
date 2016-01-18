2596c2b53a3ef2fd657a086fb000b162461ddd23
Wolfy-J <wolfy.jd@gmail.com>
2016-01-18 18:23:39 +0300

    Albus uri.

--- source/views/bundle.dark.php
+++ source/views/bundle.dark.php
@@ -6,7 +6,3 @@
 <!--Couple aliases-->
 <dark:use path="albus:elements/uri" as="albus:href"/>
 <dark:use path="albus:elements/uri" as="albus:url"/>
-
-<!--Super short alias-->
-<dark:use path="albus:elements/uri" as="uri"/>
-<dark:use path="albus:elements/uri" as="url"/>
