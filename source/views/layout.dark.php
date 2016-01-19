33a4f0dc9d05abfa5ff8116f8cfc7b0e37e892f8
Wolfy-J <wolfy.jd@gmail.com>
2016-01-19 16:18:08 +0300

    Minor CS

--- source/views/layout.dark.php
+++ source/views/layout.dark.php
@@ -1,14 +1,20 @@
 <dark:extends path="albus:layouts/root"/>
 
+<define:brand>
+    <a href="/" class="brand-logo">
+        <img src="@{basePath}resources/images/spiral.svg" alt="Spiral">
+    </a>
+</define:brand>
+
 <!--Following resources block can be redefined on a project basis-->
-<block:resources>
+<define:resources>
     <block:resources/>
-</block:resources>
+</define:resources>
 
 <!--You can replace this block to render project specific actor-->
-<block:user-block>
+<define:user-block>
     <a href="#" class="user-link">
         <?= get_class(spiral(\Spiral\Security\ActorInterface::class)) ?>
     </a>
     <a href="#" class="user-logout hide">[[Log Out]]</a>
-</block:user-block>
+</define:user-block>
