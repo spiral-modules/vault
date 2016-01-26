5a31f92e818c43546f2382a0aadbdd9095e5413d
Dmitry <zmicerboksha@gmail.com>
2016-01-26 21:27:09 +0300

    tabs hash fix

--- resources/scripts/spiral/vault.js
+++ resources/scripts/spiral/vault.js
@@ -20,6 +20,32 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     });
 
+    //add tabs to url hash
+    (function ($tabs) {
+        if (!$tabs.length) return;
+        //if (window.location.href.indexOf('#') === -1) {
+        //    var newUrl = window.location.href + $tabs[0].getAttribute('href');
+        //    history.replaceState(null, null, newUrl);
+        //    window.location.href = newUrl;
+        //}
+        $tabs.on('click', function (e) {
+            var path = window.location.href,
+                newPath;
+            if (path.indexOf('#')) {
+                path = path.substr(0, path.indexOf('#'));
+            }
+            newPath = path + e.currentTarget.getAttribute('href');
+            window.location.href = newPath;
+        });
+        window.onhashchange  = function (event) {
+            var tab = event.newURL.substr(event.newURL.indexOf('#') + 1, event.newURL.length),
+                scrollTop = document.body.scrollTop;
+            if (!$tabs.parent().find('a[href$="'+tab+'"]')) {return}
+            $tabs.parent().tabs('select_tab', tab);
+            window.scrollTo(0, scrollTop);
+        };
+    }($('.tabs a')))
+
     $(document).on('click touchstart', function (e) {
         if ($(e.target).closest('#sidenav-overlay').length) {
             $('.button-collapse').toggleClass('active');
