26affd6bc3b51de0bc8094eb4f837aaff17915e9
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-01-27 13:47:48 +0300

    fix

--- source/views/elements/grid.dark.php
+++ source/views/elements/grid.dark.php
@@ -2,7 +2,7 @@
 <dark:use bundle="vault:bundle"/>
 
 <vault:block title="${title}" node:attributes="prefix:block">
-    <spiral:grid source="${source}" as="${as}" class="${class|striped}" paginator=""
+    <spiral:grid source="${source}" as="${as}" class="${class|striped responsive-table}" paginator=""
                  empty="${empty|No results to be displayed}" empty-class="${empty-class|center}">
         ${context}
     </spiral:grid>
