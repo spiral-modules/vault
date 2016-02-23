3527d3062308c095207842a28cf953ac139b6ef5
Nataliya Zhukava <nataliya.zhukava@gmail.com>
2016-02-23 20:55:04 +0300

    fix navigation behaviour

--- resources/scripts/spiral/vault.js
+++ resources/scripts/spiral/vault.js
@@ -20,8 +20,31 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     });
 
+    //add tabs to url hash
+    (function ($tabs) {
+        if (!$tabs.length) return;
+        $tabs.on('click', function (e) {
+            if (window.location.hash === e.currentTarget.getAttribute('href')) return;
+            var path = window.location.href,
+                newPath;
+            if (path.indexOf('#')) {
+                path = path.substr(0, path.indexOf('#'));
+            }
+            newPath = path + e.currentTarget.getAttribute('href');
+            window.location.href = newPath;
+        });
+        window.onhashchange  = function (event) {
+            var tab = event.newURL.substr(event.newURL.indexOf('#') + 1, event.newURL.length),
+                scrollTop = document.body.scrollTop;
+            if (!$tabs.parent().find('a[href$="'+tab+'"]')) {return}
+            $tabs.parent().tabs('select_tab', tab);
+            window.scrollTo(0, scrollTop);
+        };
+    }($('.tabs a')))
+
     $(document).on('click touchstart', function (e) {
-        if ($(e.target).closest('#sidenav-overlay').length) {
+        console.log($(e.target));
+        if ($(e.target).closest('.drag-target').length) {
             $('.button-collapse').toggleClass('active');
         }
     });
