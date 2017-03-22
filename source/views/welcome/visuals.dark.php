ac58d316dccf1c6d407233a21e3060e9f7be81d5
iasinov <iasinov@gmail.com>
2017-03-22 15:18:36 +0300

    F pagination fix

--- source/views/welcome/visuals.dark.php
+++ source/views/welcome/visuals.dark.php
@@ -82,5 +82,17 @@
                 <grid:cell.number label="Balance:" value="<?= $item['balance'] ?>"/>
             </vault:grid>
         </tab:item>
+        <tab:item id="Elements" title="Elements" icon="list">
+            <ul class="collapsible" data-collapsible="accordion">
+                <li>
+                    <div class="collapsible-header"><span class="new badge">4</span><i class="material-icons">filter_drama</i>First</div>
+                    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                </li>
+                <li>
+                    <div class="collapsible-header"><span class="badge">1</span><i class="material-icons">place</i>Second</div>
+                    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                </li>
+            </ul>
+        </tab:item>
     </tab:wrapper>
 </block:content>
