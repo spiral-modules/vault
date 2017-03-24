306ec0a8d7c6f8e99fb937cef52d48e2dfe9bbf7
AndrewK <andrew@spiralscout.com>
2017-03-24 21:20:51 +0300

    Try converting vault in node module

--- resources/scripts/spiral/vault.js
+++ resources/scripts/spiral/vault.js
@@ -1,9 +1,9 @@
-//TODO: Polish
-$(document).ready(function () {
+// TODO: Polish
+$(document).ready(function() {
     $(".dropdown-button").dropdown();
 
     $('.collapsible').collapsible({
-        accordion: false
+        accordion: false,
     });
 
     $('ul.tabs').tabs();
@@ -12,43 +12,45 @@ $(document).ready(function () {
 
     $('.button-collapse').sideNav({
             menuWidth: 300,
-            edge: 'left'
+            edge: 'left',
         }
     );
 
-    $('.button-collapse').on('click', function () {
+    $('.button-collapse').on('click', function() {
         $(this).toggleClass('active');
     });
 
-    //add tabs to url hash
-    (function ($tabs) {
+    // Add tabs to url hash
+    (function($tabs) {
         if (!$tabs.length) return;
-        $tabs.on('click', function (e) {
+
+        $tabs.on('click', function(e) {
             if (window.location.hash === e.currentTarget.getAttribute('href')) return;
-            var path = window.location.href,
-                newPath;
+            var path = window.location.href;
             if (path.indexOf('#')) {
                 path = path.substr(0, path.indexOf('#'));
             }
-            newPath = path + e.currentTarget.getAttribute('href');
-            window.location.href = newPath;
+            window.location.href = path + e.currentTarget.getAttribute('href');
         });
-        window.onhashchange  = function (event) {
-            var tab = event.newURL.substr(event.newURL.indexOf('#') + 1, event.newURL.length),
-                scrollTop = document.body.scrollTop;
-            if (!$tabs.parent().find('a[href$="'+tab+'"]')) {return}
+
+        window.onhashchange = function(event) {
+            var tab = event.newURL.substr(event.newURL.indexOf('#') + 1, event.newURL.length);
+            var scrollTop = document.body.scrollTop;
+            if (!$tabs.parent().find('a[href$="' + tab + '"]')) {
+                return;
+            }
             $tabs.parent().tabs('select_tab', tab);
             window.scrollTo(0, scrollTop);
         };
-    }($('.tabs a')))
+    }($('.tabs a')));
 
-    $(document).on('click touchstart', function (e) {
+    $(document).on('click touchstart', function(e) {
         if ($(e.target).closest('.drag-target').length) {
             $('.button-collapse').toggleClass('active');
         }
     });
 
-    $(document).on('click', '.js-vault-confirm', function (e) {
+    $(document).on('click', '.js-vault-confirm', function(e) {
         var text = $(e.target).hasClass('js-vault-confirm')
             ? $(e.target).data('confirm')
             : $(e.target).parent('.js-vault-confirm').data('confirm');
@@ -57,4 +59,4 @@ $(document).ready(function () {
             e.stopPropagation();
         }
     });
-})
+});
