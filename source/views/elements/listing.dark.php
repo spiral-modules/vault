f8b48fb1871b5be01ba7266140849c98dbb69219
Wolfy-J <wolfy.jd@gmail.com>
2017-06-08 18:24:16 +0300

    default striped

--- source/views/elements/listing.dark.php
+++ source/views/elements/listing.dark.php
@@ -2,7 +2,7 @@
 <dark:use bundle="vault:bundle"/>
 
 <vault:block title="${title}" node:attributes="prefix:block">
-    <listing:grid listing="${listing}" as="${as}" empty="${empty|[[No results to be displayed]]}">
+    <listing:grid listing="${listing}" as="${as}" empty="${empty|[[No results to be displayed]]}" class="striped">
         ${context}
     </listing:grid>
 </vault:block>
