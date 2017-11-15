2dc31c3f4869f61b33c764832b3397ccf62a9e71
Valentin V <vvval@users.noreply.github.com>
2017-11-15 12:56:40 +0300

    Update listing.dark.php

--- source/views/elements/listing.dark.php
+++ source/views/elements/listing.dark.php
@@ -2,7 +2,11 @@
 <dark:use bundle="vault:bundle"/>
 
 <vault:block title="${title}" node:attributes="prefix:block">
-    <listing:grid listing="${listing}" as="${as}" empty="${empty|[[No results to be displayed]]}" class="striped">
-        ${context}
-    </listing:grid>
+    <div class="row">
+        <div class="col s12">
+            <listing:grid listing="${listing}" as="${as}" empty="${empty|[[No results to be displayed]]}" class="striped">
+                ${context}
+            </listing:grid>
+        </div>
+    </div>
 </vault:block>
