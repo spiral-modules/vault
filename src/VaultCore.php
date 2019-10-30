da1e54fc4db0fedde7c49774d8e8517bc1e40621
Wolfy-J <wolfy.jd@gmail.com>
2019-10-30 22:00:17 +0300

    redoing vault core

---
+++ src/VaultCore.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+declare(strict_types=1);
+
+namespace Spiral\Vault;
+
+/**
+ * Provides the protected access to the set of vault controllers and also support set of access plugins to
+ * modify the incoming controller parameters.
+ *
+ * @todo extract some functionality such as plugin support outside of vault core into external package.
+ */
+class VaultCore
+{
+
+}
--- tests/-app-/views/test.dark.php
+++
@@ -1,5 +0,0 @@
-<dark:extends path="vault:layout" title="Test" class="wide-content"/>
-
-<block:content>
-    Test vault controller
-</block:content>
